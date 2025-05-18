9601ee92bf4515d85503f9dfb66fd33a69600c1f
frostybaba <samilnurdoganforcoding@gmail.com>
2025-05-18 13:24:56 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import logging
+import os
 from app import app
 
 # Set up logging for easier debugging
 logging.basicConfig(level=logging.DEBUG)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5001, debug=True)
+    port = int(os.environ.get("PORT", 5000))  # ← bunu ekledik
+    app.run(host="0.0.0.0", port=port, debug=True)
